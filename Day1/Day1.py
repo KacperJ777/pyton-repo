80ce134dc3fd5e22e651d5a8ec441b973f38bc01
KacperJ7 <kacperjozwiak759@gmail.com>
2024-01-27 16:00:31 +0100

    Initial commit

---
+++ Day1/Day1.py
@@ -0,0 +1,31 @@
+# Find bugs, errors and debug it
+
+# print('String concatenation is done with the "+" + sign.')
+# print('e.g print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+
+# Take the input and print "Hello Kacper", to input will be assigned name value
+# print("Hello " + input("What's your name?"))
+
+# Print the length of the string
+# print(len(input("What's your name?")))
+
+# Variables
+# name = input("What's your name?")
+# length = len(name)
+# print(length)
+
+# Coding challenge movie 14, switch the values from the input and print it
+
+# a = input()
+# b = input()
+#
+#
+# temp = a
+# a = b
+# b = temp
+#
+# print("a: " + a)
+# print("b: " + b)
+
---
+++ Day1/Day1_project.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator.")
+a = input("What's name of the city you grew up in?")
+print(a)
+b = input("What's your pet's name?")
+print(b)
+print("Your band name could be " + a + " " + b)
